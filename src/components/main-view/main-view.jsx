f0ce25ca66363f4ec8c1826967f8fee902282a5e
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-05-21 16:53:29 +0200

    Added components

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            title: "The Lord of the Rings: The Return of the King",
+            image: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+            director: "Peter Jackson",
+        },
+        {
+            id: 2,
+            title: "Star Wars: Episode III - Revenge of the Sith",
+            image: "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_FMjpg_UX1000_.jpg",
+            director: "George Lucas",
+        },
+        {
+            id: 3,
+            title: "Harry Potter and the Prisoner of Azkaban",
+            image: "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg",
+            director: "Alfonso Cuarón",
+        },
+    ]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.lenght === 0) {
+        return <div>This list is empty!</div>;
+    }
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                setSelectedMovie(newSelectedMovie);
+                }}
+                />
+            ))}
+        </div>
+    );
+};
