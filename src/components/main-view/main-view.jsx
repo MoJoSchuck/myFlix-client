00c3b2509fcb6ae6ad5717c2ef1258c69e5ebb51
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-05-23 00:03:24 +0200

    Fetched API from backend project

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,49 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
-export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "The Lord of the Rings: The Return of the King",
-            image: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
-            director: "Peter Jackson",
-        },
-        {
-            id: 2,
-            title: "Star Wars: Episode III - Revenge of the Sith",
-            image: "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_FMjpg_UX1000_.jpg",
-            director: "George Lucas",
-        },
-        {
-            id: 3,
-            title: "Harry Potter and the Prisoner of Azkaban",
-            image: "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_.jpg",
-            director: "Alfonso Cuarón",
-        },
-    ]);
 
+export const MainView = () => {
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    const fetchMovies = async () => {
+        try {
+            const response = await fetch("https://afternoon-sands-47cb04422b71.herokuapp.com/movies");
+            if (!response.ok) {
+                throw new Error("Failed to fetch movies");
+            }
+            const data = await response.json();
+            setMovies(data);
+        } catch (error) {
+            console.error("Error fetching movies:", error.message);
+        }
+    };
 
     if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            <MovieView
+                movie={selectedMovie}
+                onBackClick={() => setSelectedMovie(null)}
+            />
         );
     }
 
-    if (movies.lenght === 0) {
+    if (movies.length === 0) {
         return <div>This list is empty!</div>;
     }
+
     return (
         <div>
             {movies.map((movie) => (
                 <MovieCard
-                    key={movie.id}
+                    key={movie._id}
                     movie={movie}
                     onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-                }}
+                        setSelectedMovie(newSelectedMovie);
+                    }}
                 />
             ))}
         </div>
